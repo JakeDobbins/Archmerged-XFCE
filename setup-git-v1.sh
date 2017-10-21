c6ee4949cec328dbd270b0a67e2dbecf4f8e1c3b
Jake Dobbins <hollarback@me.com>
2017-10-21 19:33:40 -0400

    Creating my first commit

---
+++ setup-git-v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Debois
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.archmerge.com
+# Website	:	https://www.archmerged.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "Jake Dobbins"
+git config --global user.email "hollarback@me.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
